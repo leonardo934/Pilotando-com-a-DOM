79929db0dd58eca4d7e2ab0b51a860d5f520676e
leonardo <leonardocave934@gmail.com>
2023-01-31 14:37:06 -0300

    innerHTML

--- Selecionando elementos/index.js
+++ Selecionando elementos/index.js
@@ -3,13 +3,19 @@ const elementUm = document.getElementById('blog-title')
 console.log(elementUm)
 
 
+
+
 const elementDois = document.getElementsByClassName('one')
 
 console.log(elementDois)
 
+
+
 const elementtres = document.getElementsByTagName('h3')
 
 console.log(elementtres)
+
+
 
 
 const elementquatro = document.querySelector('.two')
@@ -18,15 +24,28 @@ console.log(elementquatro)
 
 
 
+
+
 const elementcinco = document.querySelectorAll('.three')
 
 
 elementcinco.forEach(el => console.log(el))
 
+
+
 const elementseis = document.querySelector('.four')
 
 console.log(elementseis.textContent)
 
+
+
 const elementsete = document.querySelector('.five')
 
 elementsete.innerText = "Pão com mandioca!"
+
+
+
+
+const elementoito = document.querySelector('.six')
+
+elementoito.innerHTML = "<big>Pão</big> <small>com</small> <big>Mandioca</big> <small>e</small>  <big>Quijo</big>"
