4d496b9f612ea871d59e56b0aa44d8f3d59a89d7
leonardo <leonardocave934@gmail.com>
2023-01-31 15:04:54 -0300

    Manipulando atributos

--- Selecionando elementos/index.js
+++ Selecionando elementos/index.js
@@ -56,5 +56,13 @@ const elementnove = document.querySelector('#one')
 console.log(elementnove.value)
 elementnove.value = "Outro valor"
 
+const header = document.querySelector('header')
+header.setAttribute('id', 'header')
 
+const headerID = document.querySelector('#header')
 
+console.log(headerID.getAttribute('class'))
+
+header.removeAttribute('id')
+
+header.setAttribute('class', 'bg header') 
