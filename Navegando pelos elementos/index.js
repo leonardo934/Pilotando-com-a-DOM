9556c4cba11bed7c98367f60dbf7f8026f6a69f0
leonardo <leonardocave934@gmail.com>
2023-02-01 15:03:54 -0300

    insertBefore

--- Navegando pelos elementos/index.js
+++ Navegando pelos elementos/index.js
@@ -4,18 +4,25 @@ const el = document.querySelector('body')
 console.log(el.lastElementChild)
 
 
-const div = document.createElement('div')
-div.innerHTML = "<big>Olá</big> <small>tudo bem???</small>"
+const divum = document.createElement('div')
+divum.innerHTML = "<big>Olá</big> <small>tudo bem???</small> primeiro"
 
-const body = document.querySelector('body')
-
-
-body.prepend(div)
+const bodyum = document.querySelector('body')
 
 
+bodyum.prepend(divum)
 
 
 
+
+const divdois = document.createElement('div')
+divdois.innerHTML = "Olá <small>tudo bem???</small> segundo"
+
+const bodydois = document.querySelector('body')
+const header = bodydois.querySelector('header')
+bodydois.insertBefore(divdois,header.nextSibling)
+
+bodydois.prepend(divdois)
 
 
 
