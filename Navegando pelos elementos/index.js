419f3ca3cac0ea0db135261f0f448a3a13eab83c
leonardo <leonardocave934@gmail.com>
2023-02-01 14:53:42 -0300

    Criando e adicionando elementos na página

--- Navegando pelos elementos/index.js
+++ Navegando pelos elementos/index.js
@@ -1,9 +1,52 @@
 const el = document.querySelector('body')
 
 
-
-
-
-
-
 console.log(el.lastElementChild)
+
+
+const div = document.createElement('div')
+div.innerHTML = "<big>Olá</big> <small>tudo bem???</small>"
+
+const body = document.querySelector('body')
+
+
+body.prepend(div)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
